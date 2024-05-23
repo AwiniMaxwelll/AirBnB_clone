ae4f6703e391370adcbac722ac2df86fd99f00cd
Awinimaxwelll <awinimaxwell428@gmail.com>
2024-05-23 08:43:44 -0700

    modified

--- models/base_model.py
+++ models/base_model.py
@@ -1,43 +1,24 @@
-#!/usr/bin/python3
-"""The base model"""
-import uuid
-from datetime import datetime as dt
+#!/usr/bin/python
+""" holds class City"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
-class BaseModel:
-    """The parent parent class to all the classes"""
+
+class City(BaseModel, Base):
+    """Representation of city """
+    if models.storage_t == "db":
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities")
+    else:
+        state_id = ""
+        name = ""
+
     def __init__(self, *args, **kwargs):
-        if len(kwargs) > 0:
-            kwargs['created_at'] = dt.strptime(kwargs['created_at'],
-                                               '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['updated_at'] = dt.strptime(kwargs['updated_at'],
-                                               '%Y-%m-%dT%H:%M:%S.%f')
-            for key, value in kwargs.items():
-                if '__class__' not in key:
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = dt.now()
-            self.updated_at = dt.now()
-
-    def __str__(self):
-        """Return the string representation of the BaseModel object"""
-        return '[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__)
-
-    def __repr__(self):
-        """return the string representation of the BaseModel class"""
-        return '[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__)
-
-    def save(self):
-        """Updates <updated_at> instance with the current datetime"""
-        from models import storage
-        self.updated_at = dt.now()
-        storage.save()
-
-
-    def to_dict(self):
-        """returns a dictionary containing all keys/values of the BaseModel class"""
-        cls_dict = dict(self.__dict__)
-        cls_dict['__class__'] = self.__class__.__name__
-        cls_dict['created_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        cls_dict['updated_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        return cls_dict
+        """initializes city"""
+        super().__init__(*args, **kwargs)
